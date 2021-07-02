b3b429cd9d2d5e984ad42a659a93c479a744522c
H-2464 <h-2464@H-2464noMacBook-Pro.local>
2021-07-02 22:09:06 +0900

    Add favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from "react-helmet";
 
 function App() {
+  const string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  const random = string[Math.floor(Math.random()*string.length)]
   return (
     <div className="App">
+      <Helmet>
+        <link
+          rel="icon"
+          href={`
+            data:image/svg+xml;
+            utf8,
+            <svg enable-background="new 0 0 16 16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+              <text font-family="'Helvetica Neue', 'Helvetica', 'Arial', sans-serif" font-size="16" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">
+                ${random}
+              </text>
+            </svg>
+          `}
+        />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
